8bf489bba6026d45312f6d19fd4e2c839e6807c4
Joemon Jose <joemonjose@iMac.ultrahub>
2025-08-16 18:38:41 +0100

    Fix type error in mongodb.tsx and update connection cache logic

--- src/app/lib/mongodb.tsx
+++ src/app/lib/mongodb.tsx
@@ -22,14 +22,19 @@ let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
-}
 
+
+// Use ! only when you're sure the variable is not undefined. In this case, it's safe because you initialize it right before.
 export async function dbConnect(): Promise<Mongoose> {
-  if (cached.conn) return cached.conn;
+  if (cached!.conn) return cached!.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
+
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
+}
 
   cached.conn = await cached.promise;
   console.log("Connected to MongoDB Atlas");
