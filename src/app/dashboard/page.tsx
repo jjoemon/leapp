f693041f8e14b41dcfdd46e60702fc8d107a730e
Joemon Jose <joemonjose@iMac.ultrahub>
2025-08-19 06:50:00 +0100

    Fix type error in mongodb.tsx and update connection cache logic 3

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,32 @@
   import Image from 'next/image';
+  import { lusitana } from '@/app/ui/fonts';
 
   export default function Page() {
   return (
-    <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-      { <Image
-          src="/joemon.jpg"
-          width={1000}
-          height={760}
-          className="rounded-full object-cover"
-          alt="Screenshots of the dashboard project showing desktop version"
-      />}
-  </div>)
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-4xl`}>
+        Joemon's Dashboard
+      </h1>
+      <div className="flex flex-col md:flex-row items-center justify-center p-6 gap-6">
+    {/* Left side: image */}
+    <div className="flex items-center justify-center md:w-3/5 md:px-28 md:py-12">
+      <Image
+        src="/joemon.jpg"
+        width={1000}
+        height={760}
+        className="rounded-full object-cover"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+    </div>
+
+    {/* Right side: new data */}
+    <div className="flex-1">
+      <h3 className={`${lusitana.className} mb-4 text-xl md:text-4xl`}>
+        new data goes here
+      </h3>
+      {/* You can add more content under this */}
+    </div>
+  </div>
+
+  </main>)
 }
